0b30448efbd12322a7127502e8b629cc358ab633
O. HADALIN <dyblikat777@gmail.com>
2024-09-14 03:23:26 +0300

    Task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,24 @@
 'use strict';
-function slugify(title) {
-  const newTitle = title.split(" ");
-  let newMassive = [];
-  for (const oneWord of newTitle) {
-    newMassive.push(oneWord.toLowerCase());
+function isEnoughCapacity(products, containerSize) {
+  let sumProducts = 0;
+  for (let product in products) {
+    sumProducts += products[product];
   }
-  const slug = newMassive.join("-");
-  return slug;
+  
+  return sumProducts <= containerSize;
 }
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
